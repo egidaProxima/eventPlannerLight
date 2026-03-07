b18b0a62d50c58517fa83c0aedf0d9f4bfdf0440
egidaProxima <xr8411144@gmail.com>
2026-03-07 13:38:19 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,203 @@
+﻿class Program
+{
+    static void Main()
+    {
+        using (ApplicationContext db = new ApplicationContext())
+        {
+            ShowOptions();
+
+            while (true)
+            {
+                string[] mainEnter = Console.ReadLine()!.Split(' ');
+
+                if (mainEnter[0] == "exit")
+                {
+                    break;
+                }
+                else if (mainEnter[0] == "add")
+                {
+                    if (mainEnter[1] == "worker")
+                    {
+                        Console.WriteLine("enter Workers:  |  Age  |   Name   |   Surname   |\n");
+
+                        while (true)
+                        {
+                            string[] enter = Console.ReadLine()!.Split(' ');
+
+                            if (enter[0] == "exit")
+                            {
+                                Console.WriteLine("exit from 'add worker'");
+                                db.SaveChanges();
+                                break;
+                            }
+                            else
+                            {
+                                var newWorker = AddWorker(enter);
+                                db.Workers.Add(newWorker);
+                            }
+                        }
+                    }
+                    else if (mainEnter[1] == "event")
+                    {
+                        Console.WriteLine("enter Events:  |    Adress   |     Date     |   \n");
+
+                        while (true)
+                        {
+                            string[] enter = Console.ReadLine()!.Split(' ');
+
+                            if (enter[0] == "exit")
+                            {
+                                Console.WriteLine("exit from 'add event'");
+                                db.SaveChanges();
+                                break;
+                            }
+                            else
+                            {
+                                var newMeasure = AddMeasure(enter);
+                                db.Measures.Add(newMeasure);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"error in 2 word of command : " + mainEnter[1]);
+                    }
+                }
+                else if (mainEnter[0] == "set" && mainEnter[2] == "to")
+                {
+                    if (mainEnter[1] == "worker")
+                    {
+                        if (mainEnter[3] == "event")
+                        {
+                            Console.WriteLine("enter event ID");
+                            int eventID = int.Parse(Console.ReadLine()!);
+                            Console.WriteLine("enter workers of event with ID ", eventID);
+
+                            while (true)
+                            {
+                                string[] enter = Console.ReadLine()!.Split(' ');
+
+                                if (enter[0] == "exit")
+                                {
+                                    Console.WriteLine("exit from 'set worker to event'");
+                                    db.SaveChanges();
+                                    break;
+                                }
+                                else
+                                {
+
+                                    for (int i = 0; i < enter.Length; i++)
+                                    {
+                                        var newSchedule = new Schedule { WorkerId = int.Parse(enter[i]), MeasureId = eventID };
+                                        db.Schedules.Add(newSchedule);
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine($"error in 3 word of command : " +mainEnter[3]);
+                        }
+                    }
+                    else if (mainEnter[1] == "event")
+                    {
+                        if (mainEnter[3] == "worker")
+                        {
+
+                        }
+                        else
+                        {
+                            Console.WriteLine($"error in 3 word of command : " + mainEnter[3]);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"error in 2 word of command : " + mainEnter[2]);
+                    }
+                }
+                else if (mainEnter[0] == "cls")
+                {
+                    Console.Clear();
+                    ShowOptions();
+                }
+                else if (mainEnter[0] == "print")
+                {
+                    if (mainEnter[1] == "workers")
+                    {
+                        Console.WriteLine("Workers:  |  Age  |   Name   |   Surname   |\n");
+                        foreach (var worker in db.Workers)
+                        {
+                            Console.WriteLine(worker.Id.ToString() + "  " + worker.Name.ToString() + "  " + worker.Surname.ToString() + "  " + worker.Age.ToString());
+                        }
+                    }
+                    else if (mainEnter[1] == "events")
+                    {
+                        Console.WriteLine("Events:  |    Adress   |     Date     |   \n");
+                        foreach (var measure in db.Measures)
+                        {
+                            Console.WriteLine(measure.Id.ToString() + "  " + measure.Date.ToString() + "  " + measure.Adress.ToString());
+                        }
+                    }
+                    else if (mainEnter[1] == "schedules")
+                    {
+                        Console.WriteLine("Schedules:  | worker Id | worker Name | worker Surname | event Id | event Adress | event Date | \n");
+                        var schedulesW = db.Schedules.Join(db.Workers, // второй набор
+                            sched => sched.WorkerId, // свойство-селектор объекта из первого набора
+                            worker => worker.Id, // свойство-селектор объекта из второго набора
+                            (sched, worker) => new // результат
+                            {
+                                measureId = sched.MeasureId,
+                                workerId = worker.Id,
+                                name = worker.Name,
+                                surname = worker.Surname
+                            });
+                        var schedulesE = schedulesW.Join(db.Measures, // второй набор
+                            sched => sched.measureId, // свойство-селектор объекта из первого набора
+                            measure => measure.Id, // свойство-селектор объекта из второго набора
+                            (sched, measure) => new // результат
+                            {
+                                measureId = sched.measureId,
+                                workerId = sched.workerId,
+                                name = sched.name,
+                                surname = sched.surname,
+                                date = measure.Date,
+                                adress = measure.Adress
+                            });
+
+                        var list = schedulesE.OrderBy(e => e.workerId).ToList();
+                        foreach (var sched in list)
+                        {
+                            Console.WriteLine($"{sched.workerId} {sched.name} {sched.surname} {sched.measureId} {sched.adress} {sched.date} ");
+                        }
+                    }
+                }
+                else if (mainEnter[0] == "serach")
+                {
+
+                }
+            }
+
+            db.SaveChanges();
+        }
+    }
+    
+    public static Worker AddWorker(string[] enter)
+    {
+        Worker newWorker = new Worker {Age = int.Parse(enter[0]), Name = (string)enter[1], Surname = (string)enter[2] };
+        return newWorker;
+    }
+    public static Measure AddMeasure(string[] enter)
+    {
+        Measure newMeasure = new Measure {Adress = enter[0], Date = DateOnly.Parse(enter[1])};
+        return newMeasure;
+    }
+    public static void ShowOptions()
+    {
+        Console.WriteLine("now date : " + DateTime.Now.ToString());
+        Console.WriteLine("options: \n add - add 'something' \n print 'something'[s] - print in console (print workers) " +
+            "\n exit - end operating" +
+            "\n cls - clear console \n set - set 'something' to 'something'   (set worker to event / " +
+            "set event to worker)");
+        Console.WriteLine("entities: \n worker \n event");
+    }
+}
---
+++ dataBase.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<Worker> Workers { get; set; }
+    public DbSet<Measure> Measures { get; set; }
+    public DbSet<Schedule> Schedules { get; set; }
+    public ApplicationContext()
+    {
+        Database.EnsureCreated();
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=usersdb;Username=postgres;Password=1234");
+    }
+}
+
+
+public class Worker
+{
+    public int Id { get; set; }
+    public required int Age { get; set; }
+    public required string Name { get; set; }
+    public required string Surname { get; set; }
+}
+public class Measure
+{
+    public int Id { get; set; }
+    public required string Adress { get; set; }
+    public required DateOnly Date { get; set; }
+}
+
+[PrimaryKey(nameof(WorkerId), nameof(MeasureId))]
+public class Schedule
+{
+    public required int WorkerId { get; set; }
+    public required int MeasureId { get; set; }
+}
